de1eef291dea8c70183acb3757858f2b36df8c3d
nikitaOsipof <nikita@ifmo.spb.ru>
2025-10-30 21:40:23 +0300

    Задание 1

--- Telecommunication01/Program.cs
+++ Telecommunication01/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace Telecommunication01
+﻿using System.Text;
+
+namespace Telecommunication01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // имя и баланс счета
+            string name = "Иван Петров";
+            double balance = 100;
+
+            // действие клиента
+            char callType = 'Г';
+            int minutes = 1;
+
+            // изменение баланса в зависимости от типа звонка и длительности разговора
+            balance -= minutes * 5;
+
+            // вывод результата
+            Console.WriteLine("Клиент: {0} имеет баланс: {1}", name, balance);
+
+            // Выполняется пополнение баланса 
+            double amountPaid = 10;
+            balance += amountPaid;
+            
+            // вывод результата
+            Console.WriteLine($"Клиент: {name} имеет баланс: {balance}");
+
             Console.WriteLine("Hello, telecommunication!");
         }
     }
