3f44e0d4ebf28965df011132e8321e7b3387aaaf
nikitaOsipof <nikita@ifmo.spb.ru>
2025-11-07 22:51:12 +0300

    Применение перечисления и структуры для реализации клиента

--- Telecommunication01/Program.cs
+++ Telecommunication01/Program.cs
@@ -2,6 +2,19 @@
 
 namespace Telecommunication01
 {
+    public enum callType
+    {
+        Sity,
+        Mobile
+    }
+
+    public struct Customer
+    {
+        public string name;
+        public double balance;
+        public callType callTypeCust;
+        
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -12,12 +25,19 @@ namespace Telecommunication01
             string name = "Иван Петров";
             double balance = 100;
 
+            Customer cust;
+            cust.name = "Другой Иван Петров";
+            cust.balance = 250;
+            cust.callTypeCust = callType.Sity;
+
             // действие клиента
-            char callType = 'Г';
+           // char callType = 'Г';
+           // callType callTypeCust = callType.Sity;
             int minutes = 1;
 
             // изменение баланса в зависимости от типа звонка и длительности разговора
             balance -= minutes * 5;
+            cust.balance -= minutes * 8;
 
             // вывод результата
             Console.WriteLine("Клиент: {0} имеет баланс: {1}", name, balance);
@@ -25,11 +45,15 @@ namespace Telecommunication01
             // Выполняется пополнение баланса 
             double amountPaid = 10;
             balance += amountPaid;
+            cust.balance += amountPaid;
             
             // вывод результата
             Console.WriteLine($"Клиент: {name} имеет баланс: {balance}");
 
             Console.WriteLine("Hello, telecommunication!");
+
+            // вывод результата клиента как объекта
+            Console.WriteLine($"Клиент: {cust.name} имеет баланс: {cust.balance}");
         }
     }
 }
